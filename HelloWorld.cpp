a72fba5477040454e039292ea35522e017297f46
Ayesha Kanwal <ayeshakanwal2468@gmail.com>
2025-01-13 23:21:48 +0500

    percentage of subjects

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,13 +1,23 @@
 #include<iostream>
 using namespace std;
-int main(){
-    cout<<"Hello World"<<endl;
 
-    int x=5;
-    int y=6;
-    cout<<x<<endl;
-    cout<<y<<endl;
+int main(){
+
+//     cout<<"Hello World"<<endl;
+
+//     float x=6;
+//     int y=6;
+//     cout<<x<<endl;
+//     cout<<x/y<<endl;
+    float a = 6;
+    float b = 7;
+    float c = 8;
+    float d = 9;
+    float e = 5;
+
+    cout<<"Percentage of subjects: "<<(a+b+c+d+e)/5 <<"%";
 
 
     return 0;
+
 }
